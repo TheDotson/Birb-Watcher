c85855ff78cebb021a03d5e56e6d5fa0c7740f5d
Michael Dotson <thedotson@icloud.com>
2020-08-29 10:00:38 -0500

    setup file structure and added working Auth func

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import fbConnection from '../helpers/data/connection';
+import EditBirb from '../components/pages/EditBirb/EditBirb';
+import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import NewBirb from '../components/pages/NewBirb/NewBirb';
+import SingleBirb from '../components/pages/SingleBirb/SingleBirb';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
-        <h1>Scat-Surprise</h1>
-        <button className="btn btn-info">
-        <i className="fas fa-surprise"></i> Surprise? <i className="fas fa-surprise"></i></button>
+        <MyNavbar authed={authed}/>
+        <h1>Birb-Watcher</h1>
+        <Home/>
+        <EditBirb/>
+        <NewBirb/>
+        <SingleBirb/>
       </div>
     );
   }
