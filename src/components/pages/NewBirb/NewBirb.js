fe6d9b0cf25701afbc3537a08b062cff56a29995
Michael Dotson <thedotson@icloud.com>
2020-09-01 19:10:45 -0500

    create birb in progress

--- src/components/pages/NewBirb/NewBirb.js
+++ src/components/pages/NewBirb/NewBirb.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import ReactDatePicker from 'react-datepicker';
 
 class NewBirb extends React.Component {
+  state = {
+    type: '',
+    color: '',
+    size: '',
+    seenAt: new Date(),
+    altColor: '',
+    wasSleeping: true,
+    location: '',
+    notes: '',
+  }
+
+  changeTypeEvent = (e) => {
+    e.preventDefault();
+    this.setState({ type: e.target.value });
+  }
+
+  seenAtEvent = (seenAt) => {
+    this.setState({ seenAt });
+  }
+
+  saveBirb = (e) => {
+    e.preventDefault();
+    
+  }
+
   render() {
+    const { type, seenAt } = this.state;
     return (
       <div className="NewBirb">
         <h1>NewBirb</h1>
+        <form className="col-6 offset-3">
+          <div className="form-group">
+            <label htmlfor="">Type</label>
+            <input
+            type="text"
+            classname="form-control"
+            id="birbType"
+            placeholder="Enter Birb Type"
+            value={type}
+            onChange={this.changeTypeEvent}
+            />
+          </div>
+          <div>
+            <label htmlfor="birbSeenAt">Seen At: </label>
+            <ReactDatePicker
+              selected={seenAt}
+              onChange={this.seenAtEvent}
+              showTimeSelect
+            />
+          </div>
+        </form>
       </div>
     );
   }
